e1d478d26d75e122fa8a2623d4cb86f37ce12686
Jungmin Kim <vencottkim@gmail.com>
2019-11-11 19:23:18 +0900

    데이터 구조

--- app/src/main/java/com/mediaproj/momo/data/User.java
+++ app/src/main/java/com/mediaproj/momo/data/User.java
@@ -8,6 +8,54 @@ public class User {
     private int age;
     private boolean gender; // T-남
     private String name;
+    private Section section;
+    private Genre genre;
 
+    public User(String email, String profileImg, String password, String nickname, int age, boolean gender, String name, Section section, Genre genre) {
+        this.email = email;
+        this.profileImg = profileImg;
+        this.password = password;
+        this.nickname = nickname;
+        this.age = age;
+        this.gender = gender;
+        this.name = name;
+        this.section = section;
+        this.genre = genre;
+    }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getProfileImg() {
+        return profileImg;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Section getSection() {
+        return section;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
 }
