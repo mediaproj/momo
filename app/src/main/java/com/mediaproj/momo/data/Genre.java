00601c126ce9dc96e57c064b2d12bfb375722f8a
Jungmin Kim <vencottkim@gmail.com>
2019-11-11 19:23:26 +0900

    데이터 구조

--- app/src/main/java/com/mediaproj/momo/data/Genre.java
+++ app/src/main/java/com/mediaproj/momo/data/Genre.java
@@ -13,4 +13,67 @@ public class Genre {
     private boolean family;
     private boolean sf;
     private boolean animation;
+
+    public Genre(boolean drama, boolean fantasy, boolean horror, boolean romance, boolean action, boolean thriller, boolean blues, boolean mistery, boolean comedy, boolean family, boolean sf, boolean animation) {
+        this.drama = drama;
+        this.fantasy = fantasy;
+        this.horror = horror;
+        this.romance = romance;
+        this.action = action;
+        this.thriller = thriller;
+        this.blues = blues;
+        this.mistery = mistery;
+        this.comedy = comedy;
+        this.family = family;
+        this.sf = sf;
+        this.animation = animation;
+    }
+
+    public boolean isDrama() {
+        return drama;
+    }
+
+    public boolean isFantasy() {
+        return fantasy;
+    }
+
+    public boolean isHorror() {
+        return horror;
+    }
+
+    public boolean isRomance() {
+        return romance;
+    }
+
+    public boolean isAction() {
+        return action;
+    }
+
+    public boolean isThriller() {
+        return thriller;
+    }
+
+    public boolean isBlues() {
+        return blues;
+    }
+
+    public boolean isMistery() {
+        return mistery;
+    }
+
+    public boolean isComedy() {
+        return comedy;
+    }
+
+    public boolean isFamily() {
+        return family;
+    }
+
+    public boolean isSf() {
+        return sf;
+    }
+
+    public boolean isAnimation() {
+        return animation;
+    }
 }
--- app/src/main/java/com/mediaproj/momo/data/Section.java
+++ app/src/main/java/com/mediaproj/momo/data/Section.java
@@ -5,4 +5,27 @@ public class Section {
     private boolean musical;
     private boolean show;
     private boolean festival;
+
+    public Section(boolean movie, boolean musical, boolean show, boolean festival) {
+        this.movie = movie;
+        this.musical = musical;
+        this.show = show;
+        this.festival = festival;
+    }
+
+    public boolean isMovie() {
+        return movie;
+    }
+
+    public boolean isMusical() {
+        return musical;
+    }
+
+    public boolean isShow() {
+        return show;
+    }
+
+    public boolean isFestival() {
+        return festival;
+    }
 }
